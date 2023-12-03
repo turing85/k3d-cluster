ff025022c71e5c2cec24037bbd41f19bbcc0f503
Marco Bungart <marco.bungart@googlemail.com>
2023-12-03 13:42:17 +0100

    move logs of docker, k3s, ... in dedicated files

--- scripts/create-k3d-cluster.sh
+++ scripts/create-k3d-cluster.sh
@@ -11,9 +11,14 @@ function waitFor() {
 }
 
 cd -- "$(dirname "$(readlink -f "$0")")" &> /dev/null
+log_dir="../logs"
+mkdir -p ${log_dir}
+rm -rf ${log_dir}/
 
 k3d_version="$(k3d --version | head -n 1 | sed 's|k3d version v\(.*\)$*|\1|g')"
 
+mkdir -p ${log_dir}/docker
+echo "starting local registry"
 docker run \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Headers='[Authorization,Accept,Cache-Control]' \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Methods='["*"]' \
@@ -34,8 +39,14 @@ docker run \
   --label k3s.registry.port.internal=5000 \
   --restart=unless-stopped \
   --detach \
-  docker.io/library/registry:2.8.2 2>/dev/null \
-  || docker start local.registry.localhost
+  docker.io/library/registry:2.8.2 \
+  1>${log_dir}/docker/docker_local_registry.log \
+  2>${log_dir}/docker/docker_local_registry.err.log \
+  || docker start local.registry.localhost \
+    1>${log_dir}/docker/docker_local_registry.log \
+    2>${log_dir}/docker/docker_local_registry.err.log
+
+echo "starting docker registry mirror"
 docker run \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Headers='[Authorization,Accept,Cache-Control]' \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Methods='["*"]' \
@@ -57,7 +68,13 @@ docker run \
   --restart=unless-stopped \
   --detach \
   docker.io/library/registry:2.8.2 2>/dev/null \
-  || docker start docker.mirror.registry.localhost
+  1>${log_dir}/docker/docker_docker_registry.log \
+  2>${log_dir}/docker/docker_docker_registry.err.log \
+  || docker start docker.mirror.registry.localhost \
+    1>${log_dir}/docker/docker_docker_registry.log \
+    2>${log_dir}/docker/docker_docker_registry.err.log \
+
+echo "starting quay registry mirror"
 docker run \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Headers='[Authorization,Accept,Cache-Control]' \
   --env REGISTRY_HTTP_HEADERS_Access-Control-Allow-Methods='["*"]' \
@@ -79,45 +96,57 @@ docker run \
   --restart=unless-stopped \
   --detach \
   docker.io/library/registry:2.8.2 2>/dev/null \
-  || docker start quay.mirror.registry.localhost
+  1>${log_dir}/docker/docker_quay_registry.log \
+  2>${log_dir}/docker/docker_quay_registry.err.log \
+  || docker start quay.mirror.registry.localhost \
+    1>${log_dir}/docker/docker_quay_registry.log \
+    2>${log_dir}/docker/docker_quay_registry.err.log \
 
+echo "starting k3s"
 k3d cluster create local \
   --config ../k3d-local.yml \
-  --registry-config ../k3d-registry.yml
+  --registry-config ../k3d-registry.yml \
+  1>${log_dir}/k3s.log \
+  2>${log_dir}/k3s.err.log \
 
 kubeconfig="${K3D_KUBECONFIG:-${HOME}/.kube/config.k3d}"
 echo "" "${kubeconfig}"
-k3d kubeconfig merge local --output "${kubeconfig}"
-kubectl ctx k3d-local
+k3d kubeconfig merge local --output "${kubeconfig}" 1>/dev/null
+kubectl ctx k3d-local 1>/dev/null
 
-helm repo add traefik https://traefik.github.io/charts || \
-  helm repo update traefik
-printf "Trying to uninstall traefik"
-waitFor 2m \
-  "helm uninstall traefik -n kube-system"
-echo " Done!"
-helm uninstall traefik-crd -n kube-system
+mkdir -p ${log_dir}/helm
+echo "Installing traefik through helm"
+helm repo add traefik https://traefik.github.io/charts 1>/dev/null || \
+  helm repo update traefik 1>/dev/null
 helm install \
   traefik traefik/traefik \
   --values ../traefik/helm-values.yml \
   --namespace traefik \
-  --create-namespace
+  --create-namespace \
+  1>${log_dir}/helm/traefik.log \
+  2>${log_dir}/helm/traefik.err.log
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts \
-  || helm repo update prometheus-community
+echo "Installing prometheus through helm"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts 1>/dev/null \
+  || helm repo update prometheus-community 1>/dev/null
 helm install \
   prometheus-operator prometheus-community/kube-prometheus-stack \
   --values ../prometheus/helm-values.yml \
   --namespace monitoring \
-  --create-namespace
+  --create-namespace \
+  1>${log_dir}/helm/prometheus.log \
+  2>${log_dir}/helm/prometheus.err.log
 
-helm repo add argocd https://argoproj.github.io/argo-helm \
-  || helm repo update argo-cd
+echo "Installing argocd through helm"
+helm repo add argocd https://argoproj.github.io/argo-helm 1>/dev/null \
+  || helm repo update argo-cd 1>/dev/null
 helm install \
   argocd argo-cd/argo-cd \
   --values ../argocd/helm-values.yml \
   --namespace argocd \
-  --create-namespace
+  --create-namespace \
+  1>${log_dir}/helm/argocd.log \
+  2>${log_dir}/helm/argocd.err.log
 
 printf "Waiting for ArgoCD to become available"
 waitFor 2m \
@@ -137,23 +166,37 @@ waitFor 2m \
     argocd.k3d.localhost"
 echo " Done!"
 
+mkdir -p ${log_dir}/argocd
+echo "Re-installing argocd through argocd"
 kubectl apply \
   --filename ../argocd/argocd-project-system.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/argocd.log \
+  2>${log_dir}/argocd/argocd.err.log
 
 kubectl apply \
   --filename ../argocd/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/argocd.log \
+  2>${log_dir}/argocd/argocd.err.log
 
+echo "Re-installing traefik through argocd"
 kubectl apply \
   --filename ../traefik/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/traefik.log \
+  2>${log_dir}/argocd/traefik.err.log
 
+echo "Installing cert-manager through argocd"
 ../cert-manager/certs/build-certs.sh
-kubectl create namespace cert-manager
+kubectl create namespace cert-manager \
+  1>${log_dir}/argocd/cert-manager.log \
+  2>${log_dir}/argocd/cert-manager.err.log
 kubectl apply \
   --filename ../cert-manager/helm/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/cert-manager.log \
+  2>${log_dir}/argocd/cert-manager.err.log
 printf "Deploying Certificate chain for cert-manager"
 waitFor 2m \
   "kubectl apply \
@@ -162,26 +205,40 @@ waitFor 2m \
 echo " Done!"
 kubectl apply \
   --filename ../cert-manager/default-tls-crt/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/cert-manager.log \
+  2>${log_dir}/argocd/cert-manager.err.log
 
+echo "Re-installing prometheus through argocd"
 kubectl apply \
   --filename ../prometheus/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/prometheus.log \
+  2>${log_dir}/argocd/prometheus.err.log
 
+echo "Installing kubernetes-dashboard through argocd"
 kubectl create namespace kubernetes-dashboard
 kubectl apply \
   --filename ../kubernetes-dashboard/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/kubernetes-dashboard.log \
+  2>${log_dir}/argocd/kubernetes-dashboard.err.log
 
+echo "Installing UIs for image registres through argocd"
 kubectl create namespace registry-ui
 kubectl apply \
   --filename ../registry-ui/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/registry-ui.log \
+  2>${log_dir}/argocd/registry-ui.err.log
 
+echo "Installing portainer through argocd"
 kubectl create namespace portainer
 kubectl apply \
   --filename ../portainer/argocd-application.yml \
-  --namespace argocd
+  --namespace argocd \
+  1>${log_dir}/argocd/portainer.log \
+  2>${log_dir}/argocd/portainer.err.log
 
 urls=( \
   "https://dashboard.k3d.localhost" \
@@ -206,7 +263,7 @@ do
   echo " Done!"
 done
 
-
+echo
 echo "Cluster is started up!"
 echo
 echo "The following Services are provided out of the box:"
@@ -220,10 +277,13 @@ echo "    https://docker.registry.k3d.localhost (dockerhub mirror)"
 echo "    https://quay.registry.k3d.localhost (quay.io mirror)"
 echo
 echo "The system  provides a registry"
-echo "    local.registry.localhost"
+echo "    local.registry.localhost:5000"
 echo
 echo "If you want to deploy custom images, push them into this registry."
 echo "To deploy the, reference them as local.registry.localhost:5000/<image-name>:<image-tag>"
 echo
 echo "All endpoints (except for the registry) are TLS-secured by default. You can find the self-signed CA certificate under"
-echo "    cert-manager/certs/ca/ca.crt
+echo "    cert-manager/certs/ca/ca.crt"
+echo
+echo "The kubernetes configuration has been written to"
+echo "    ${kubeconfig}"
